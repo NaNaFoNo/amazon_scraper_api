e6d28f38df51c7d0eba3790e1172946bfc372688
Andreas Vieten <cryptonari@outlook.com>
2022-06-15 20:27:23 +0200

    add: scraper api test

--- index.js
+++ index.js
@@ -6,11 +6,25 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const apiKey = process.env.SCRAPER_API_KEY;
+const baseURL = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true"`
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Welcome to Amazon Scraper API');
 });
+
+// GET Product Details
+app.get('/products/:productId', async (req, res) => {
+    const  { productId } = req.params;
+
+    try {
+        const response = await request(`${baseURL}&url=https://www.amazon.com/dp/${productId}`);
+
+        res.json(JSON.parse(response));
+    } catch (error) {
+        res.json(error);
+    }
+})
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
